6c36595d07bd8b076cbfadde6ca55233ed75f407
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-07-20 03:23:34 +0100

    Release 0.1.7

--- dromedary/version.py
+++ dromedary/version.py
@@ -17,10 +17,10 @@
 
 """Version information for dromedary."""
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 # Version information for dromedary
-version_info = (0, 1, 6, "final", 0)
+version_info = (0, 1, 7, "final", 0)
 
 
 def _format_version_tuple(version_info: tuple) -> str:
